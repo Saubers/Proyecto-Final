f389ff347433705f13e60236bfa7489ff7b07a1f
Bautista Echaide <81939312+EchaideBauti@users.noreply.github.com>
2021-09-28 12:26:13 -0300

    Create ProductCard.jsx

---
+++ client/src/components/ProductCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ProductCard = ({name,marca,img}) =>{
+    return(
+    <div>
+        <div>
+            <h1>{name}</h1>
+            <h3>{marca}</h3>
+        </div>
+        <div>
+            <img src={img} alt="img" />
+        </div>
+    </div>
+    );
+};
+
+export default ProductCard;
