80c65ba8ae58122908b3986d6653a8c20587dd2d
Bautista Echaide <81939312+EchaideBauti@users.noreply.github.com>
2021-09-28 16:37:40 -0300

    CreateProduct

--- api/src/controllers/carsFunction.js
+++ api/src/controllers/carsFunction.js
@@ -27,7 +27,7 @@ const GetAllCards = async (req,res,next) => {
     try {
         const GetAll = await card.findAll({
             include:{
-                model:Card,
+                model:Cars,
                 atributes:["name","marca"]
             }
         })
@@ -37,6 +37,24 @@ const GetAllCards = async (req,res,next) => {
         res.status(404).send(error)
     }
     
+}
+//S25 Crear ruta para crear/agregar Producto
+const CreateProduct = async (req,res,next) => {
+    try{
+        const {name,marca,description,img,category,feature} = req.body;
+
+        const NewProduct = await Cars.create({
+            name,
+            marca,
+            description,
+            img,
+            category,
+            feature
+        });
+        res.status(200).json(NewProduct)
+    }catch(error){
+        next(error);
+    }
 }
 
 module.exports = {
