96f98bbc885d0009737d705e5d8681fd0cfb9545
Bautista Echaide <81939312+EchaideBauti@users.noreply.github.com>
2021-09-29 18:22:48 -0300

    Style navbar y searchbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import styleNav from '../NavBar/NavBar.module.css';
+import logo from '../image/logo.png';
 
 const NavBar = () => {
     return (
-        <div >
-            <nav>
+        <div className={styleNav.container}>
+            <nav className={styleNav.navstyle}>
                 <div>
-                    <span>LogoAuto</span>
+                    <img src={logo} alt="logo" width="50px"/>
                 </div>
-        
-                <div>
-                    <li><a href="# ">Home</a></li>
-                    <li><a href="# ">Crear</a></li>
+              
+                <div className={styleNav.divli}>
+                    <li><a className={styleNav.styleA} href="# ">New Cars</a></li>
+                    <li><a className={styleNav.styleA}href="# ">Use Cars</a></li>
                 </div>
             </nav>
         </div>
--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 //import { getByName } from "../actions";
+import styleSearch from '../SearchBar/SearchBar.module.css';
 export default function SearchBar(){
 const dispatch = useDispatch()
 const [name,setName] = useState("")
@@ -19,14 +20,23 @@ function handleSubmit(e){
 }
 
 return(
-    <div >
-        <form onSubmit={e => handleSubmit(e)}>
-            <input
-            type ="text"
-            placeholder ='Busqueda por nombre'
-            onChange ={e=> handleInputChange(e)}
-            />
-            <button type = 'submit'> Buscar</button>
+    <div className={styleSearch.containerSearch}>
+        <form onSubmit={e => handleSubmit(e)} className={styleSearch.styleform}>
+            <div className={styleSearch.divprin}>
+                <div className={styleSearch.divsecu}>
+                    <h2 className={styleSearch.h2}>Find your favorite car</h2>
+                </div>
+                <div>
+                    <input className={styleSearch.inputbusqueda}
+                    type ="text"
+                    placeholder ='Busqueda por nombre'
+                    onChange ={e=> handleInputChange(e)}
+                    />
+                    <button className={styleSearch.btn} type = 'submit'> Buscar</button>
+                </div>
+                
+            </div>
+            
         </form>
     </div>
 )
