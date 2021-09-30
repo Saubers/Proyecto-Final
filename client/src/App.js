832d7990d3735867aca15be03ee52cabba69e692
Saubers <laserna.seba@gmail.com>
2021-09-30 11:46:50 -0300

    estilos

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Catalogo from './components/Catalogo/Catalogo'
 import ProductCard from './components/ProductCard/ProductCard'
 import Home from './components/Home/Home';
+import LandingPage from './components/LandingPage/LandingPage';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path="/" component={LandingPage}/>
         <Route exact path="/home" component={Home}/>
         <Route exact path="/home/catalogo" component={Catalogo}/>
         <Route path = "/home/:id" component= {ProductCard}/>
---
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import styles from '../LandingPage/LangingPage.module.css'
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+export default function LandingPage() {
+return(
+       <div className={styles.landing}>
+        <h1 className={styles.title}>Car Shop</h1>
+        <Link to ='/home'>
+            <button className={styles.button}>ENTER THE STORE</button>
+        </Link>
+       </div>
+      )
+}
