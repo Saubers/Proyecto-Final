ef95600a5f9bf43689721fc25547b1e5a85b2fa7
Juan Gaitan <diablogaitan2@gmail.com>
2021-09-30 10:03:32 -0300

    Creacion de Home
    No terminado

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import {Route,Switch} from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Catalogo from './components/Catalogo/Catalogo'
 import ProductCard from './components/ProductCard/ProductCard'
+import Home from './components/Home/Home';
 
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/home" component={Catalogo}/>
+        <Route exact path="/home" component={Home}/>
         <Route path = "/home/:id" component= {ProductCard}/>
         <Route path="/" component={Catalogo}/>
       </Switch>
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import SearchBar from "../SearchBar/SearchBar";
+import NavBar from '../NavBar/NavBar'
+import styleHome from '../Home/Home.module.css'
+
+export default function Home(){
+
+return(
+<div className = {styleHome.container}>
+    <NavBar/>
+    <SearchBar/>
+    <h1>Buy your first car</h1>
+    <div>
+        <h5>New 0km models</h5>
+        {/*imagenes de autos 0km*/}
+        <Link to = '/'><h6>Catalogo</h6></Link>
+        <h6>Payment Methods</h6>
+        {/*imagenes de visa mastercard y tatata*/}
+        <h6>Branch offices</h6>
+        {/*imagenes Maps*/}
+        <Link to = '/contact'><h6>Contact</h6></Link>
+        
+    </div>
+</div>
+)
+}
