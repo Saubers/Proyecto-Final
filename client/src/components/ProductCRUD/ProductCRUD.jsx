1b1488979b3fe58e02ff00d4728e24f47f5bea34
Lucio Gomez <luciotony25@gmail.com>
2021-10-01 19:53:31 -0300

    crud

--- client/src/components/ProductCRUD/ProductCRUD.jsx
+++ client/src/components/ProductCRUD/ProductCRUD.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
-import {postProduct} from "../actions/index.js";
+import {postProduct} from '../../actions/index'
 import {useDispatch, useSelector} from "react-redux";
 
 export default function ProductCRUD(){
