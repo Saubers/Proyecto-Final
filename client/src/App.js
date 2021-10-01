437f8f89f8d398a11f1aac96f4e10e735d8f197d
Juan Gaitan <diablogaitan2@gmail.com>
2021-10-01 15:50:21 -0300

    Comenzando con componenente contacto

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import {Route,Switch} from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Catalogo from './components/Catalogo/Catalogo'
 import ProductDetail from './components/ProductDetail/ProductDetail'
+import Contact from './components/Contacto/Contacto';
 import Home from './components/Home/Home';
 import LandingPage from './components/LandingPage/LandingPage';
 
@@ -14,7 +15,8 @@ function App() {
         <Route exact path="/" component={LandingPage}/>
         <Route exact path="/home" component={Home}/>
         <Route exact path="/home/catalogo" component={Catalogo}/>
-        <Route path = "/home/:id" component= {ProductDetail}/>
+        <Route exact path = "/home/:id" component= {ProductDetail}/>
+        <Route path = "/contactos" component= {Contact}/>
       </Switch>
     </Router>
   );
