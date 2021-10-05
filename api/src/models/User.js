2d5019a0e21cae48dbff53933093bd409884f2b6
Lucio Gomez <luciotony25@gmail.com>
2021-10-04 22:16:48 -0300

    Update User.js

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -1,23 +1,55 @@
-const {Schema ,model} = require('mongoose')
-
-const UserSchema = new Schema({
-    name :{
-        type : String,
-        required: true
+const { Schema, model } = require("mongoose");
+//const bcrypt = require("bcryptjs");
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    mail : {
-        type : String,
-        required: true
+    lastName: {
+      type: String,
+      required: true,
     },
-    password : {
-        type : String,
-        required: true
+    image: {
+      type: String, // todavia falta deducir como mandan la foto desde el front
     },
-    status : {
-        type : String,
-        required: true
+    phone: {
+      type: String,
     },
-    })
-
-
-module.exports = model('User', UserSchema)
+    mail: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    ban: {
+      type: Boolean,
+      default: false,
+    },
+    promoted: {
+      type: Boolean,
+      default: false,
+    },
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+       unique: true
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    reported: {
+      type: Boolean,
+      default: false,
+    },
+},
+  {
+    timestamps: true, // timestamps para que nos cargue fecha de ser creado y de actualizado si las hay
+  }
+);
+module.exports = model("User", userSchema);
