2e857cbfcb9d815a896fd473ef464bfc199453bd
Lucio Gomez <luciotony25@gmail.com>
2021-10-06 01:02:57 -0300

    rutass

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {idCars, GetAllCars, CreateProduct,DeleteCar,ModifiCar, SearchCars, carBrands, uploadFile} = require('../controllers/carsFunction');
 const { CreateCategory, DeleteCategory, ModifiCategory, getByCategory,GetAllCategories} = require('../controllers/categoriesFunction.js')
+const { agregarOrden,AllOrders,OrdenesByUsuario,cartOrderId,putCart, deleteCart,CartUser} = require('../controllers/cartFunctions')
 // const {getUsers, createUser} = require('../controllers/userFunction')
 module.exports = app => {
     router.get('/', function(req, res){
@@ -48,9 +49,30 @@ module.exports = app => {
 
 
 
+    ///////Carrito
+
+    ///todos los items del Carrito con query 
+    router.get('/orders',AllOrders)
+    
+
+    /// GET users/:idUser/cart
+    router.get('/users/:idUser/cart',CartUser)
 
 
+    //Ruta para agregar Item al Carrito
+    router.post('/users/:idUser/cart', agregarOrden)
 
+    ///modificar
+    router.put('/orders/:id',putCart)
+
+    //Orden en particular 
+    router.get('/orders/:id',cartOrderId)
+
+    //Ordener de usuario
+    router.get('/users/:id/orders', OrdenesByUsuario)
+
+    //Vaciar carrito
+    router.get('/users/:idUser/cart/',deleteCart)
 
     app.use(router);
 }
