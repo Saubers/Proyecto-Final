eb59edc718bf3589db6ca68db29bb0c2cc1baea2
Bautista Echaide <81939312+EchaideBauti@users.noreply.github.com>
2021-10-06 10:29:58 -0300

    a

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -7,17 +7,16 @@ import styles from '../Auth/Auth.module.css'
 
 export default function Register () {
     const user = useSelector((state) => state.user);
-    const [email, setEmail] = useState('')
+    const [mail, setMail] = useState('')
     const [password, setPassword] = useState('')
-    const [name, setName] = useState('')
-    const [lastName, setlastName] = useState('')
+    const [fullname, setFullname] = useState('')
     const [phone, setPhone] = useState('')
 
     const dispatch = useDispatch()
 
     function handleSubmit(e){
         e.preventDefault()
-        dispatch(userRegister(email,password,name,phone,lastName))
+        dispatch(userRegister(mail,password,fullname,phone))
         console.log("creado")
     }
 
@@ -27,12 +26,10 @@ export default function Register () {
             <NavBar />
             <div className={styles.login}>
                 <form action="/register" method="POST" onSubmit={handleSubmit} >
-                    <label htmlFor='name'>Nombre</label>
-                    <input type="name" name="name"  value={name} onChange={(e) => setName(e.target.value)} />
-                    <label htmlFor='lastname'>Apellido</label>
-                    <input  type="lastname" name="lastName"  value={lastName} onChange={(e) => setlastName(e.target.value)} />
+                    <label htmlFor='name'>Nombre completo</label>
+                    <input type="name" name="fullname"  value={fullname} onChange={(e) => setFullname(e.target.value)} />
                     <label  htmlFor='email'>Correo electrónico</label>
-                    <input  type="email" name="email"  value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input  type="email" name="mail"  value={mail} onChange={(e) => setMail(e.target.value)} />
                     <label htmlFor='password'>Contraseña</label>
                     <input type="password" name="password"  value={password} onChange={(e) => setPassword(e.target.value)} />
                     <label htmlFor='phone'>Telefono</label>
