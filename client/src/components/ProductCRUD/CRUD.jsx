baf095f5dc1dc1b40988779561651a9363239c70
Victorio Tocalini <victorio.ismael10@gmail.com>
2021-10-12 12:23:37 -0300

    CRUD un poco mas lindo

--- client/src/components/ProductCRUD/CRUD.jsx
+++ client/src/components/ProductCRUD/CRUD.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
+import styleCRUD from './CRUD.module.css'
 
 export default function CRUD() {
     return (
@@ -8,13 +9,22 @@ export default function CRUD() {
             <div>
                 <NavBar />
             </div>
-            <Link to='CRUD/CreateProduct'><h1>.Create</h1></Link>
-            <Link to='/home/Catalogo'><h1>.Read</h1></Link>
-            <Link to='CRUD/UpdateProduct'><h1>.Update</h1></Link>
-            <Link to='CRUD/DeleteProduct'><h1>.Delete</h1></Link>
-            <Link to='/home'>
-                <button>Back</button>
-            </Link>
+            <crud>
+                <ul>
+                    <li>
+                        <Link to='CRUD/CreateProduct'><h1>Create</h1></Link>
+                    </li>
+                    <li>
+                        <Link to='/home/Catalogo'><h1>Read</h1></Link>
+                    </li>
+                    <li>
+                        <Link to='CRUD/UpdateProduct'><h1>Update</h1></Link>
+                    </li>
+                    <li>
+                        <Link to='CRUD/DeleteProduct'><h1>Delete</h1></Link>
+                    </li> 
+                </ul>
+            </crud>
         </div>
     )
 }
