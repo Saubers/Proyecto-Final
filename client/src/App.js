44b073e8657591f525c9bc4529a93d8a9eb1e0f4
Sebastián Laserna <laserna.seba@gmail.com>
2021-10-14 23:00:34 -0300

    arreglo login y home

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ const local = localStorage.getItem('userInfo')
     <Router>
       <div className='container'>
       <Switch>
-{ !local && ( <Route exact path="/" component={LandingPage} />)}
+ <Route exact path="/" component={LandingPage} />
         <Route exact path="/home/Catalogo/:id" component={ProductDetail} />
         <Route exact path="/home/CrearCategoria" component={CategoryCreate} />
    { local ?  (<Route exact path='/CRUD' component={CRUD} />)
