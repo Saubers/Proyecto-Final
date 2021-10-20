8c3bb1d3d62d4bdc83e1e06f3a474b77f6fb9ff8
Leandro Farias <leandrodavidfarias@gmail.com>
2021-10-20 01:32:14 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,23 @@ import Catalogo from './components/Catalogo/Catalogo';
 import ProductDetail from './components/ProductDetail/ProductDetail';
 import Contact from './components/Contacto/Contacto';
 import LandingPage from './components/LandingPage/LandingPage';
-import CategoryCreate from './components/CategoryCreate/CategoryCreate';
 import Register from './components/Auth/register/Register';
 import Cart from './components/Cart/Cart';
 import Login from './components/Auth/login/Login';
-import CRUD from './components/ProductCRUD/CRUD.jsx';
+
+import ProductCRUD from './components/ProductCRUD/ProductCRUD'
 import CreateProduct from './components/ProductCRUD/CRUD_Components/CreateProduct/CreateProduct';
 import UpdateProduct from './components/ProductCRUD/CRUD_Components/UpdateProduct/UpdateProduct';
 import DeleteProduct from './components/ProductCRUD/CRUD_Components/DeleteProduct/DeleteProduct';
+import ReadProduct from './components/ProductCRUD/CRUD_Components/ReadProduct/ReadProduct';
+import ReadProductDetail from './components/ProductCRUD/CRUD_Components/ReadProduct/ReadProductDetail';
+
+import CategoryCRUD from './components/CategoryCRUD/CategoryCRUD'
+import CategoryCreate from './components/CategoryCRUD/CategoryCreate/CategoryCreate';
+import CategoryUpdate from './components/CategoryCRUD/CategoryUpdate/CategoryUpdate';
+import CategoryDelete from './components/CategoryCRUD/CategoryDelete/CategoryDelete';
+import CategoryRead from './components/CategoryCRUD/CategoryRead/CategoryRead'
+
 import OrderCar from './components/OrderCars/OrderCar';
 import { useHistory } from 'react-router';
 import Profile from './components/ProfileInfo/Profile';
@@ -21,33 +30,57 @@ import ProfileInfo from './components/UserInfo/UserInfo'
 
 function App() {
   const history = useHistory()
-const local = localStorage.getItem('userInfo')
-const isAdmin = localStorage.getItem('userAdmin')
-console.log(isAdmin)
+  const local = localStorage.getItem('userInfo')
+  const isAdmin = localStorage.getItem('userAdmin')
+  console.log(isAdmin)
   return (
     <Router>
       <div className='container'>
-      <Switch>
- <Route exact path="/" component={LandingPage} />
-       {isAdmin !== '"inactive"' && <Route exact path="/home/Catalogo/:id" component={ProductDetail} />}
-        {isAdmin !== '"inactive"' && <Route exact path="/home/CrearCategoria" component={CategoryCreate} />}
-   { isAdmin === '"admin"' &&  (<Route exact path='/CRUD' component={CRUD} />)
-   
-  }
-      { isAdmin === '"admin"' && <Route exact path='/CRUD/CreateProduct' component={CreateProduct} />}
-      { isAdmin === '"admin"' && <Route exact path='/CRUD/DeleteProduct' component={DeleteProduct} />}
-      { isAdmin === '"admin"' && <Route exact path='/CRUD/UpdateProduct' component={UpdateProduct} />}
-{ !local && (<Route exact path='/user/register' component={Register} />)}
-      <Route exact path="/home/Catalogo" component={Catalogo} />
-{ !local &&  (<Route exact path="/user/login" component={Login} />)}
-{local && (<Route exact path='/user/profile' component={Profile} />)}
-{  isAdmin !== '"inactive"' &&  ( <Route exact path='/home/compra' component={Cart} />)}
-       {isAdmin !== '"inactive"' &&  <Route exact path='/home/ADMIN/orders' component={OrderCar}/>}
-        <Route exact path='/user/me' component={ProfileInfo} />
-       {isAdmin === '"admin"' && <Route exact path='/home/ADMIN/orders/:id' component={OrderDetail}/>}
-        <Route exact path="/contactos" component={Contact} />
-        <Route path="/pagos" component={Pagos} />
-      </Switch>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/home/Catalogo/:id" component={ProductDetail} />
+
+
+          {isAdmin === '"admin"' && <Route exact path='/ProductCRUD' component={ProductCRUD} />}
+          {isAdmin === '"admin"' && <Route exact path='/CRUD/CreateProduct' component={CreateProduct} />}
+          {isAdmin === '"admin"' && <Route exact path='/CRUD/DeleteProduct' component={DeleteProduct} />}
+          {isAdmin === '"admin"' && <Route exact path='/CRUD/UpdateProduct' component={UpdateProduct} />}
+          {isAdmin === '"admin"' && <Route exact path='/ProductCRUD/ReadProduct' component={ReadProduct} />}
+          {isAdmin === '"admin"' && <Route exact path='/ProductCRUD/ReadProduct/:id' component={ReadProductDetail} />}
+
+          {/* <Route exact path='/ProductCRUD' component={ProductCRUD} />
+          <Route exact path='/ProductCRUD/CreateProduct' component={CreateProduct} />
+          <Route exact path='/ProductCRUD/DeleteProduct' component={DeleteProduct} />
+          <Route exact path='/ProductCRUD/UpdateProduct' component={UpdateProduct} />
+          <Route exact path='/ProductCRUD/ReadProduct' component={ReadProduct} />
+          <Route exact path='/ProductCRUD/ReadProduct/:id' component={ReadProductDetail} /> */}
+
+
+          {isAdmin === '"admin"' && <Route exact path="/CategoryCRUD" component={CategoryCRUD} />}
+          {isAdmin === '"admin"' && <Route exact path="/CategoryCRUD/CrearCategoria" component={CategoryCreate} />}
+          {isAdmin === '"admin"' && <Route exact path="/CategoryCRUD/CategoryUpdate" component={CategoryUpdate} />}
+          {isAdmin === '"admin"' && <Route exact path="/CategoryCRUD/CategoryDelete" component={CategoryDelete} />}
+          {isAdmin === '"admin"' && <Route exact path="/CategoryCRUD/CategoryRead" component={CategoryRead} />}
+
+
+          {/* <Route exact path="/CategoryCRUD" component={CategoryCRUD} />
+          <Route exact path="/CategoryCRUD/CrearCategoria" component={CategoryCreate} />
+          <Route exact path="/CategoryCRUD/CategoryUpdate" component={CategoryUpdate} />
+          <Route exact path="/CategoryCRUD/CategoryDelete" component={CategoryDelete} />
+          <Route exact path="/CategoryCRUD/CategoryRead" component={CategoryRead} /> */}
+
+
+          {!local && (<Route exact path='/user/register' component={Register} />)}
+          <Route exact path="/home/Catalogo" component={Catalogo} />
+          {!local && (<Route exact path="/user/login" component={Login} />)}
+          {local && (<Route exact path='/user/profile' component={Profile} />)}
+          {(<Route exact path='/home/compra' component={Cart} />)}
+          <Route exact path='/home/ADMIN/orders' component={OrderCar} />
+          <Route exact path='/user/me' component={ProfileInfo} />
+          <Route exact path='/home/ADMIN/orders/:id' component={OrderDetail} />
+          <Route exact path="/contactos" component={Contact} />
+          <Route path="/pagos" component={Pagos} />
+        </Switch>
       </div>
     </Router>
   );
